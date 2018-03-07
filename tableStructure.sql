48f6bef552369eda3c9c8e9da652ed175d19e16f
wasdkiller <kushan.gunasekera@gmail.com>
2018-03-07 23:42:44 +0530

    Table Structure of the Database

---
+++ tableStructure.sql
@@ -0,0 +1,37 @@
+CREATE TABLE IF NOT EXISTS allusers(
+	userid VARCHAR(15),
+	fname VARCHAR(20),
+	lname VARCHAR(20),
+	uname VARCHAR(20),
+	PRIMARY KEY (userid)
+);
+
+CREATE TABLE IF NOT EXISTS groups(
+	groupid VARCHAR(15),
+	title VARCHAR(50),
+	welcomeMessage VARCHAR(255),
+	stickerPermission VARCHAR(5),
+  hhhPermission VARCHAR(5),
+	PRIMARY KEY (groupid)
+);
+
+CREATE TABLE IF NOT EXISTS subscribe(
+	subsname VARCHAR(20),
+	userid VARCHAR(15),
+	PRIMARY KEY (subsname),
+  FOREIGN KEY (userid) REFERENCES allusers(userid)
+);
+
+CREATE TABLE IF NOT EXISTS superadmin(
+	userid VARCHAR(15),
+	PRIMARY KEY (userid),
+	FOREIGN KEY (userid) REFERENCES allusers(userid)
+);
+
+CREATE TABLE IF NOT EXISTS users(
+  groupid VARCHAR(15),
+	userid VARCHAR(15),
+	PRIMARY KEY (groupid, userid),
+  FOREIGN KEY (groupid) REFERENCES groups(groupid),
+	FOREIGN KEY (userid) REFERENCES allusers(userid)
+);
