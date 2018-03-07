9f7f9cdaa1e633924798a1de6e028270195b13c3
wasdkiller <kushan.gunasekera@gmail.com>
2018-03-07 23:37:33 +0530

    Handling all the Contact type messages

---
+++ contactHandler.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import configuration
+import common
+import dbFunction
+
+admin = configuration.admin
+
+def privateContact(bot, message):
+    if(message.from_user.id == admin and message.reply_to_message != None):
+        try:
+            if (message.contact.last_name != None):
+                bot.send_contact(chat_id=message.reply_to_message.forward_from.id, phone_number=message.contact.phone_number, first_name=message.contact.first_name, last_name=message.contact.last_name)
+            else:
+                bot.send_contact(chat_id=message.reply_to_message.forward_from.id, phone_number=message.contact.phone_number, first_name=message.contact.first_name)
+        except:
+            print('Cannot send message to pm user')
+        return
+    if(message.from_user.id != admin):
+        bot.forward_message(chat_id=admin, from_chat_id=message.chat.id, message_id=message.message_id)
+
+def replyToContact(bot, message, types):
+    if(dbFunction.isAvailable(message.chat.id, message.reply_to_message.from_user.id)):
+        try:
+            bot.send_message(chat_id=message.reply_to_message.from_user.id,
+                             text= common.getName(message.from_user) + ' @ <b>' + message.chat.title + '</b> : reply as a Contact',
+                             parse_mode='HTML')
+            if (message.contact.last_name != None):
+                bot.send_contact(chat_id=message.reply_to_message.from_user.id, phone_number=message.contact.phone_number, first_name=message.contact.first_name, last_name=message.contact.last_name)
+            else:
+                bot.send_contact(chat_id=message.reply_to_message.from_user.id, phone_number=message.contact.phone_number, first_name=message.contact.first_name)
+        except:
+            common.exceptionHandling(message, bot, types, message.from_user)
