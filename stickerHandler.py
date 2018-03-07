d01f9c3ab51a7589fb390e05b27f102768000936
wasdkiller <kushan.gunasekera@gmail.com>
2018-03-07 23:41:56 +0530

    Handling all the Sticker type messages

---
+++ stickerHandler.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import configuration
+import common
+import dbFunction
+
+admin = configuration.admin
+
+def privateSticker(bot, message):
+    if (message.from_user.id == admin and message.reply_to_message != None):
+        try:
+            bot.send_message(chat_id=message.reply_to_message.forward_from.id, text=message.text, parse_mode='HTML')
+        except:
+            print('Cannot send message to pm user')
+        return
+    if (message.from_user.id != admin):
+        bot.send_message(chat_id=admin, text='>>> private sticker send by ' + common.getName(message.from_user))
+        bot.forward_message(chat_id=admin, from_chat_id=message.chat.id, message_id=message.message_id)
+
+def deleteSticker(bot, message):
+    if(not(dbFunction.getStickerPermission(message.chat.id))):
+        if(common.isBotAdmin(bot, message)):
+            try:
+                bot.delete_message(message.chat.id, message.message_id)
+            except:
+                print('Sticker Delete Failed')
+        else:
+            for chat in bot.get_chat_administrators(message.chat.id):
+                if(chat.user.is_bot==False):
+                    try:
+                        bot.send_message(chat_id=chat.user.id, text='Cannot delete Stickers in <b>' + message.chat.title + '</b>\n* Please make me as an admin or Enable my Delete Message Permission', parse_mode='HTML')
+                    except:
+                        print('failed to send message to group admin')
