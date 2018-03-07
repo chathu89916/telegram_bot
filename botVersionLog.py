a653c26ff561cf7ab7564164329910af573560f2
wasdkiller <kushan.gunasekera@gmail.com>
2018-03-07 23:36:31 +0530

    Bot Versions history

---
+++ botVersionLog.py
@@ -0,0 +1,38 @@
+def botVesion():
+    BOT_VERSION = """\n\n<b>What's New in Version 2.1</b>
+
+* If someone mention you or your subscribe word in a reply text, you will be notified."""
+
+    return BOT_VERSION
+
+
+def changeLOG():
+    changeLOG = """\n\n<b>Version 2.1</b>
+
+* If someone mention you or your subscribe word in a reply text, you will be notified.
+    
+<b>Version 2.0</b>
+
+* /subscribe, /unsubscribe and /subscribelist commands introduced ( just type / and follow the introduction )
+
+* Group admins can use /strickerpemission and /hhhpermission commands introduced ( just type / and follow the introduction )
+    
+<b>Version 1.3.0</b>
+
+* You can't play Games in <b>ResistanceLK</b> anymore.
+
+<b>Version 1.2.2</b>
+
+* When someone mention you in a caption of a photo or reply to your message with a photo you can get it in high quality.
+
+<b>Version 1.2.1</b>
+
+* When you use stickers in <b>ResistanceLK</b>, it will be automatically deleted by the BOT.
+
+<b>Version 1.1.0</b>
+
+* When you reply to a message with a photo, video, audio, document, contact, voice or location it will be informed you as usual.
+
+* If someone mention you in a caption of a photo, video, audio, document or voice that will be also informed. """
+
+    return changeLOG
