4001e451acfbfdafbc4f1202cfc0b45d88693dbc
Kushan Harshana Gunasekera <kushan.gunasekera@gmail.com>
2018-07-31 12:18:13 +0530

    kik message not sending when bot left

--- botFunctions/leftChatMemberHandler.py
+++ botFunctions/leftChatMemberHandler.py
@@ -3,13 +3,15 @@ import botFunctions
 import configuration
 
 admin = configuration.admin
+botID = configuration.botID
 
 def leftMember(message):
     botFunctions.leftOfKikMember(message.chat.id, message.left_chat_member.id)
 
 def kikBot(bot, message):
     if(botFunctions.kikBotDB(message.chat.id)=='success'):
-        bot.send_message(chat_id=admin, text='Successfully kik me from <b>' + str(message.chat.title) + '</b> '+ str(message.chat.type) + ' by ' + botFunctions.getName(message.from_user), parse_mode='HTML')
+        if(botID!=message.from_user.id):
+            bot.send_message(chat_id=admin, text='Successfully kik me from <b>' + message.chat.title + '</b> '+ message.chat.type + ' by ' + botFunctions.getName(message.from_user), parse_mode='HTML')
     else:
         bot.send_message(chat_id=admin,
-                         text='Failed to kik me from <b>' + str(message.chat.title) + '</b> '+ str(message.chat.type) + ' by ' + botFunctions.getName(message.from_user), parse_mode='HTML')
+                         text='Failed to kik me from <b>' + message.chat.title + '</b> '+ message.chat.type + ' by ' + botFunctions.getName(message.from_user), parse_mode='HTML')
