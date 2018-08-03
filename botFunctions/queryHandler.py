2f6af22e18911e0d39985a1768929ac54c0bc701
Kushan Harshana Gunasekera <kushan.gunasekera@gmail.com>
2018-08-03 16:29:07 +0530

    simplify the superAdminHandler() function

--- botFunctions/queryHandler.py
+++ botFunctions/queryHandler.py
@@ -2,20 +2,15 @@
 import botFunctions
 
 def superAdminHandler(bot, types, call):
-    print("superAdminHandler")
     crossIcon = u"\u274C"
     markup = types.InlineKeyboardMarkup()
     superAdminList = botFunctions.detailsOfSuperAdmins()
     for adminID in superAdminList:
-        print(adminID)
-        adminDetails = adminID[1]
-        if(adminID[2]!=''):
-            adminDetails = adminDetails + adminID[2]
+        adminDetails = botFunctions.userDetailFormatter(adminID)
         if(adminID[3]!=''):
-            adminDetails = adminDetails + ' ---> @' +adminID[3]
-            markup.add(types.InlineKeyboardButton(text=adminDetails, url='https://telegram.me/'+adminID[3]+'?start=XXXX'), types.InlineKeyboardButton(text=crossIcon, url='https://telegram.me/'+adminID[3]+'?start=XXXX'))
+            markup.add(types.InlineKeyboardButton(text=adminDetails, url='https://telegram.me/'+adminID[3]+'?start=XXXX'), types.InlineKeyboardButton(text=crossIcon, callback_data="['superadmin',"+str(adminID[0])+"]"))
         else:
-            markup.add(types.InlineKeyboardButton(text=adminDetails,  callback_data='noUserName'), types.InlineKeyboardButton(text=crossIcon, url='https://telegram.me/'+adminID[3]+'?start=XXXX'))
+            markup.add(types.InlineKeyboardButton(text=adminDetails,  callback_data='noUserName'), types.InlineKeyboardButton(text=crossIcon, callback_data="['superadmin',"+str(adminID[0])+"]"))
     markup.add(types.InlineKeyboardButton("< back", callback_data="backToHome"))
     bot.edit_message_text(chat_id=call.message.chat.id, text="<b>Super Admin List</b>", message_id=call.message.message_id, reply_markup=markup, parse_mode='HTML')
 
